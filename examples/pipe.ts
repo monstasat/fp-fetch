a954e5d828e4c34ee8c7b8e921b2ff7480fd2cb8
monstasat <sashayanin@gmail.com>
2020-05-05 17:02:51 +0300

    chore: rename TaskEither to FetchTask in pipe example

--- examples/pipe.ts
+++ examples/pipe.ts
@@ -1,9 +1,9 @@
 import { pipe } from 'fp-ts/lib/pipeable'
-import { TaskEither, chain } from 'fp-ts/lib/TaskEither';
+import { chain } from 'fp-ts/lib/TaskEither';
 
-import { FetchError, fetchText, fetchJSON } from '../src';
+import { FetchTask, fetchText, fetchJSON } from '../src';
 
-const result: TaskEither<FetchError, string> = pipe(
+const result: FetchTask<unknown, string> = pipe(
   fetchText('https://example.com'),
   chain((body) => fetchJSON('https://example.com', { body })),
   chain((body) => fetchText('https://example.com', { body: JSON.stringify(body) }))
